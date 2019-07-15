6d39a04506a81f8fc372ba3a6532377e9dc916c4
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-15 17:12:59 -0400

    initial files and config

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAVcVypjnMBvFCRZO06A_yQ4DYY45RlX_4",
+    authDomain: "hello-d0fed.firebaseapp.com",
+    databaseURL: "https://hello-d0fed.firebaseio.com",
+    projectId: "hello-d0fed",
+    storageBucket: "hello-d0fed.appspot.com",
+    messagingSenderId: "873317010849",
+    appId: "1:873317010849:web:83744c5d12971b04"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
